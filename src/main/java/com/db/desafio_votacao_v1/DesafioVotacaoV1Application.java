538c1aee291fd9df80d00472c122b38296e24641
Marcos Roberto <marcos.dj@gmail.com>
2026-03-26 23:09:57 -0300

    Criação de estrutura do projeto, entidades e enum. Além de arquivos de propriedades

---
+++ src/main/java/com/db/desafio_votacao_v1/DesafioVotacaoV1Application.java
@@ -0,0 +1,13 @@
+package com.db.desafio_votacao_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioVotacaoV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioVotacaoV1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/db/desafio_votacao_v1/domain/Pauta.java
@@ -0,0 +1,55 @@
+package com.db.desafio_votacao_v1.domain;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_pauta")
+public class Pauta implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idPauta")
+    private Long id;
+
+    @Column(name = "conteudo")
+    private String conteudo;
+
+    @OneToMany(mappedBy = "pauta", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @Column(name = "votos")
+    private List<Voto> votos;
+
+    public Pauta() {}
+
+    public Pauta(String conteudo, List<Voto> votos) {
+        this.conteudo = conteudo;
+        this.votos = votos;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public List<Voto> getVotos() {
+        return votos;
+    }
+
+    public void setVotos(List<Voto> votos) {
+        this.votos = votos;
+    }
+}
